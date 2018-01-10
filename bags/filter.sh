94ff50ef5ef355a6e0ab334f71ef812fc2c7463b
albertkasdorf <albert.kasdorf@live.de>
2018-01-10 09:25:53 +0100

    Gefilterte Bag-Dateien komprimiert.

--- bags/filter.sh
+++ bags/filter.sh
@@ -3,3 +3,6 @@
 # $1 <in-bag>
 # $2 <out-bag>
 rosbag filter $1 $2 '(topic != "/tf" and topic != "/tf_static") or topic == "/tf" and m.transforms[0].header.frame_id != "odom" and m.transforms[0].child_frame_id != "base_link"'
+
+# A backup of each bag file (with the extension .orig.bag) is made before the bag is compressed. Use LZ4 to compress data.
+rosbag compress --lz4 $2
